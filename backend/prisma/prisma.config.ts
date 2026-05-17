794d34fb08d3712f5dae0caba58b3c0d1b5b590f
BaroBaro1 <baraabahrih@gmail.com>
2026-05-17 14:03:58 +0100

    fix package json

--- backend/prisma/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-export const prisma = new PrismaClient({
-  adapter: {
-    type: 'mysql',
-    url: 'mysql://root:@localhost:3306/order_platform'
-    // إذا عندك كلمة مرور، استبدلها root:كلمة_المرور
-  }
-});
